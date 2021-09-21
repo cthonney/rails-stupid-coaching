99574fa49c0332df6f2598cea82c5c44ce996cdf
cedric thonney <c.thonney@gmail.com>
2021-09-21 20:59:24 +0400

    stupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #Write a simple route to serve the GET /ask HTTP request to the ask action of the questions controller.
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: "questions#answer"
 end
